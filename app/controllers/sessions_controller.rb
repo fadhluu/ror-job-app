e3319ca54e49750b348ff50e2e6bb1a051679558
fadhluu <fadhlurahman.i@gmail.com>
2019-12-10 09:07:24 +0700

    add login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+
+    if @user&.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
   end
 
   def login; end
 
   def welcome; end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   resources :users, only: %i[new create]
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   # get 'welcome', to: 'sessions#welcome'
 
   # homepage
