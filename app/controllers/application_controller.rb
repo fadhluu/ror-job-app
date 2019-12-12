0d2266256eec1bbb11d7722fb043d90ddb04efca
fadhlu <fadhlurahman.i@gmail.com>
2019-12-13 00:23:27 +0700

    add error msg if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
   helper_method :logged_in?
+  helper_method :check_current_user
 
   def current_user
     @user = User.find_by(id: session[:user_id])
@@ -11,5 +12,12 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     !current_user.nil?
+  end
+
+  def check_current_user
+    if current_user.blank?
+      flash[:error] = "Please Login First"
+      redirect_to login_path
+    end
   end
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,6 @@
 class JobsController < ApplicationController
+    before_action :check_current_user, only: %i[show]
+
     def show
         @job = Job.find(params[:id])
     end
