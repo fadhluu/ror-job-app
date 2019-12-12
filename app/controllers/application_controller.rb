269047238e5ec3e8a3dd02cbaad5ca433895da7f
fadhlu <fadhlurahman.i@gmail.com>
2019-12-13 01:41:42 +0700

    fixing messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,19 @@
 
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user
-  helper_method :logged_in?
-  helper_method :check_current_user
+  helper_method :current_user, :logged_in?, :check_current_user
 
   def current_user
     @user = User.find_by(id: session[:user_id])
   end
 
   def logged_in?
-    !current_user.nil?
+    !current_user.blank?
   end
 
   def check_current_user
     if current_user.blank?
-      flash[:error] = "Please Login First"
+      flash[:danger] = "Please Login First"
       redirect_to login_path
     end
   end
